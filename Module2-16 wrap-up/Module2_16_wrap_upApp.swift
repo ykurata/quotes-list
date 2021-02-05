1594d649cab0037c35bac212bd0d92182850938a
Yasuko Kurata <yasukokurata@gmail.com>
2021-02-05 15:02:16 -0500

    initial commit

---
+++ Module2-16 wrap-up/Module2_16_wrap_upApp.swift
@@ -0,0 +1,17 @@
+//
+//  Module2_16_wrap_upApp.swift
+//  Module2-16 wrap-up
+//
+//  Created by Yasuko Kurata on 2021-02-05.
+//
+
+import SwiftUI
+
+@main
+struct Module2_16_wrap_upApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
